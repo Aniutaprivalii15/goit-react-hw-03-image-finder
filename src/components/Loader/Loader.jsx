02ca989ceb06b7f1b264928244a02af63ee98733
Anna <anytka141528@gmail.com>
2023-04-12 00:13:42 +0300

    fixed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,32 @@
 import { Dna } from 'react-loader-spinner';
-import css from './Loader.module.css';
+// import css from './Loader.module.css';
 
-export const Loader = () => (
-  <div className={css.Loader}>
-    <Dna
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="dna-loading"
-      wrapperStyle={{}}
-      wrapperClass="dna-wrapper"
-    />
-  </div>
-);
+// export const Loader = () => (
+//   <div className={css.Loader}>
+//     <Dna
+//       visible={true}
+//       height="80"
+//       width="180"
+//       ariaLabel="dna-loading"
+//       wrapperStyle={{margin: '0 auto', marginTop: '200px'}}
+//       wrapperClass="dna-wrapper"
+//     />
+//   </div>
+// );
+
+export const Loader = () => {
+  return (
+    <>
+      <Dna
+        visible={true}
+        height="80"
+        width="180"
+        ariaLabel="dna-loading"
+        wrapperStyle={{ margin: '0 auto', marginTop: '300px' }}
+        wrapperClass="MagnifyingGlass-wrapper"
+        glassColor="#c0efff"
+        color="#5c5c5c"
+      />
+    </>
+  );
+};
