8460e895d70353f731bd24ae03fdf7ccda418bdb
Anna <anytka141528@gmail.com>
2023-04-12 00:18:12 +0300

    hm 3

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,4 @@
 import { Dna } from 'react-loader-spinner';
-// import css from './Loader.module.css';
-
-// export const Loader = () => (
-//   <div className={css.Loader}>
-//     <Dna
-//       visible={true}
-//       height="80"
-//       width="180"
-//       ariaLabel="dna-loading"
-//       wrapperStyle={{margin: '0 auto', marginTop: '200px'}}
-//       wrapperClass="dna-wrapper"
-//     />
-//   </div>
-// );
 
 export const Loader = () => {
   return (
@@ -23,9 +9,7 @@ export const Loader = () => {
         width="180"
         ariaLabel="dna-loading"
         wrapperStyle={{ margin: '0 auto', marginTop: '300px' }}
-        wrapperClass="MagnifyingGlass-wrapper"
-        glassColor="#c0efff"
-        color="#5c5c5c"
+        wrapperClass="dna-wrapper"
       />
     </>
   );
